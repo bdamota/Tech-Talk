4ec172e39c951d774fd34b06133d712a2182b904
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-21 22:33:39 -0700

    helpers added

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
