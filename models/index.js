9693017d5bfd3d151617ed74f26eee1e3c6fd499
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-23 10:41:40 -0700

    model index updated

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./User.js');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
 
 // create associations
 User.hasMany(Post, {
