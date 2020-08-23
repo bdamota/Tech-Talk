36e9e6adcd05cbd6b2c4ad2f0f14f43e4b84ccce
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-22 21:55:32 -0700

    comment async function fixed

--- models/post.js
+++ models/post.js
@@ -1,31 +1,9 @@
+  
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create the Post model
-class Post extends Model {
-    static upvote(body, models) {
-        return models.Vote.create({
-            user_id: body.user_id,
-            post_id: body.post_id
-        })
-        .then(() => {
-            return Post.findOne({
-                where: {
-                    id: body.post_id
-                },
-                attributes: [
-                    'id',
-                    'post_url',
-                    'title',
-                    'created_at',
-                    [
-                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'
-                    ]                    
-                ]
-            });
-        });
-    }
-}
+class Post extends Model {}
 
 // create fields/columns for Post model
 Post.init(
@@ -40,12 +18,9 @@ Post.init(
             type: DataTypes.STRING,
             allowNull: false
         },
-        post_url: {
+        post: {
             type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isURL: true
-            }
+            allowNull: false
         },
         user_id: {
             type: DataTypes.INTEGER,
--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,46 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-
-class Comment extends Model {}
-
-Comment.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        comment_text: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'post',
-                key: 'id'
-            }
+async function commentFormHandler(event) {
+    event.preventDefault();
+  
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+  
+    const post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+          method: 'POST',
+          body: JSON.stringify({
+            post_id,
+            comment_text
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      
+        if (response.ok) {
+          document.location.reload();
+        } else {
+          alert(response.statusText);
         }
-    },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment'
-    }
-);
-
-module.exports = Comment;
+      }
+  }
+  
+  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
