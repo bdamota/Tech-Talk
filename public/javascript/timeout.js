89e9ed49407f6aa035eb4ef6b283eb34b435fd27
LYNN DAMOTA <bdamota0506@gmail.com>
2020-09-05 00:40:39 -0700

    add-post js removed from homepage. error no longer running

--- public/javascript/timeout.js
+++ public/javascript/timeout.js
@@ -1,11 +1,11 @@
-import { IdleSessionTimeout } from "idle-session-timeout";
+// import { IdleSessionTimeout } from "idle-session-timeout";
  
-//time out in 5 min on inactivity
-let session = new IdleSessionTimeout(1 * 60 * 1000, "click", "mousemove");
+// //time out in 5 min on inactivity
+// let session = new IdleSessionTimeout(1 * 60 * 1000, "click", "mousemove");
  
-session.onTimeOut = () => {
-// here you can call your server to log out the user
-    console.log("timeOut");
-};
+// session.onTimeOut = () => {
+// // here you can call your server to log out the user
+//     console.log("timeOut");
+// };
 
-session.onTimeOut();
+// session.onTimeOut();
